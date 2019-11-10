70ae6b0b19ed8cffbd0dc591d6696144ed1e87f4
nomad-mystic <nomadmystics@gmail.com>
2019-11-09 18:54:28 -0800

    master: Init commit

---
+++ config/webpack.base.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+  return {
+    entry: path.resolve(__dirname, '../src/js/index.js'),
+    output: {
+      path: path.resolve(__dirname, '../../dist'),
+      filename: 'js/bundle.js'
+    },
+    watch: env.development,
+    devtool: 'eval',
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].css',
+                outputPath: './css',
+                sourceMap: env.development,
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: path.resolve(__dirname, '../'),
+                  ctx: {
+                    cssnano: env.production,
+                  },
+                },
+                sourceMap: env.development,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {sourceMap: env.development},
+            },
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          }
+        }
+      ],
+    },
+    plugins: [],
+  };
+};
---
+++ config/webpack.production.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = require('./webpack.base.config');
+
+module.exports = ( env = 'production' ) => merge(baseConfig(env), {
+  mode: 'production',
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+  ],
+});
